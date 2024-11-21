bea44b843b513a377e439a9dbf7cdb54102c9822
marcosocana-al <marcosocana.al@gmail.com>
2024-11-21 23:48:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-document.getElementById('presentation-link').href = 'https://docs.google.com/presentation/d/1B4AbN7YdeVOZVFsBrOX9ZsBpeIuiE-uQN_gjsvVP9fM/edit?usp=sharing';
+document.getElementById('link-30-days').href = 'https://docs.google.com/presentation/d/1B4AbN7YdeVOZVFsBrOX9ZsBpeIuiE-uQN_gjsvVP9fM/edit?usp=sharing';
+document.getElementById('link-famille').href = 'https://www.canva.com/design/DAGXIiLteUg/rQ8mqucyW1fDl3OLkLefqQ/edit?utm_content=DAGXIiLteUg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton';
