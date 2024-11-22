bfa03d63dfde686013fa7887b4416f62cc25346b
marcosocana-al <marcosocana.al@gmail.com>
2024-11-22 01:01:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('link-30-days').setAttribute('href', 'https://docs.google.com/presentation/d/1B4AbN7YdeVOZVFsBrOX9ZsBpeIuiE-uQN_gjsvVP9fM/edit?usp=sharing');
-document.getElementById('link-30-days').setAttribute('target', '_blank');
-document.getElementById('link-famille').setAttribute('href', 'https://www.canva.com/design/DAGXIiLteUg/rQ8mqucyW1fDl3OLkLefqQ/edit?utm_content=DAGXIiLteUg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
-document.getElementById('link-famille').setAttribute('target', '_blank');
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('link-30-days').setAttribute('href', 'https://docs.google.com/presentation/d/1B4AbN7YdeVOZVFsBrOX9ZsBpeIuiE-uQN_gjsvVP9fM/edit?usp=sharing');
+  document.getElementById('link-30-days').setAttribute('target', '_blank');
+  document.getElementById('link-famille').setAttribute('href', 'https://www.canva.com/design/DAGXIiLteUg/rQ8mqucyW1fDl3OLkLefqQ/edit?utm_content=DAGXIiLteUg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
+  document.getElementById('link-famille').setAttribute('target', '_blank');
+});
