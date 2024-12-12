49f243a5482205c0134a75eb7436cd45988e3008
marcosocana-al <marcosocana.al@gmail.com>
2024-12-12 01:48:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('link-30-days').setAttribute('target', '_blank');
   document.getElementById('link-famille').setAttribute('href', 'https://www.canva.com/design/DAGXIiLteUg/rQ8mqucyW1fDl3OLkLefqQ/edit?utm_content=DAGXIiLteUg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton');
   document.getElementById('link-famille').setAttribute('target', '_blank');
+  document.getElementById('link-nouvelles').setAttribute('href', 'https://docs.google.com/presentation/d/1vYz5Cnm8Xn6hp-uA42TkBen1CvyZefg-R69ZIhxXDT0/edit?usp=sharing');
+  document.getElementById('link-nouvelles').setAttribute('target', '_blank');
 });
